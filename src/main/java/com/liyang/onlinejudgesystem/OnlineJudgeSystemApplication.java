1f4eec795ce86fe482a36b08f113165c1feabc16
LiyangSong <song_ly@outlook.com>
2024-09-11 18:15:46 +0800

    Initialize spring boot project

---
+++ src/main/java/com/liyang/onlinejudgesystem/OnlineJudgeSystemApplication.java
@@ -0,0 +1,13 @@
+package com.liyang.onlinejudgesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineJudgeSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineJudgeSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/liyang/onlinejudgesystem/OnlineJudgeSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liyang.onlinejudgesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineJudgeSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
