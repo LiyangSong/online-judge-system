0c3ec786b050acc1162c346d2a79eb183d28234c
LiyangSong <song_ly@outlook.com>
2024-09-12 22:10:48 +0800

    add question submission entity

---
+++ src/main/java/com/liyang/onlinejudgesystem/entity/QuestionSubmission.java
@@ -0,0 +1,49 @@
+package com.liyang.onlinejudgesystem.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Date;
+import java.util.Scanner;
+
+@Entity
+@Table(name = "questionSubmissions")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class QuestionSubmission {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 128)
+    private String language;
+
+    @Column(nullable = false)
+    private String code;
+
+    private String judgeInfo;
+
+    @Column(nullable = false)
+    private int status = 0;
+
+    @Column(nullable = false, updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createTime = new Date();
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date updateTime = new Date();
+
+    @Column(nullable = false)
+    private Boolean isDeleted = false;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "questionId", nullable = false)
+    private Question question;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "userId", nullable = false)
+    private User user;
+}
