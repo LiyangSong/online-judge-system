5cc605c84bef96792cd6991a707c9a954204c103
LiyangSong <song_ly@outlook.com>
2024-10-15 22:38:10 +0800

    implement role based authorities

--- src/main/java/com/liyang/onlinejudgesystem/security/JwtUserDetailsService.java
+++ src/main/java/com/liyang/onlinejudgesystem/security/JwtUserDetailsService.java
@@ -3,28 +3,35 @@ package com.liyang.onlinejudgesystem.security;
 import com.liyang.onlinejudgesystem.repository.UserRepository;
 import com.liyang.onlinejudgesystem.entity.User;
 import lombok.AllArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 @Service
 @AllArgsConstructor
-public class UserDetailsServiceImpl implements UserDetailsService {
+public class JwtUserDetailsService implements UserDetailsService {
 
     private final UserRepository userRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findByUserAccount(username)
+        User user = userRepository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found"));
 
+        SimpleGrantedAuthority authority = new SimpleGrantedAuthority(user.getUserRole());
+        Set<GrantedAuthority> authorities = new HashSet<>();
+        authorities.add(authority);
+
         return new org.springframework.security.core.userdetails.User(
-                user.getUserAccount(),
+                user.getUsername(),
                 user.getUserPassword(),
-                new ArrayList<>()
+                authorities
         );
     }
 }
